e59ece3e7c192e79f8241c48bd2fcffdbf2da2ac
lendry02 <marrero0812@gmail.com>
2021-09-30 19:49:50 -0400

    Aplicacion de permiso  terminada

--- App2/app/src/main/java/com/example/apppermisos/MainActivity.java
+++ App2/app/src/main/java/com/example/apppermisos/MainActivity.java
@@ -1,21 +1,116 @@
 package com.example.apppermisos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private final int permRequestCode = 200;
+    private ArrayList<String> permisos = new ArrayList<>();
+    Switch opLoc,opCar,opSto,opCon,opPho;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        opLoc = findViewById(R.id.opLocation);
+        opCar = findViewById(R.id.opCamara);
+        opSto = findViewById(R.id.opStorage);
+        opPho = findViewById(R.id.opPhone);
+        opCon = findViewById(R.id.opContacts);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        verificarPermisos();
+    }
+
+    private boolean checkedPermission(String permiso){
+        int resultpermiss = ContextCompat.checkSelfPermission(getApplicationContext(),permiso);
+        return resultpermiss == PackageManager.PERMISSION_GRANTED ;
+    }
+
+    private void requestPermission(String[] permiso){
+        ActivityCompat.requestPermissions(this,permiso,permRequestCode);
     }
 
     public void SendPermiss(View view){
+
+        if(opLoc.isChecked() && !checkedPermission("android.permission.ACCESS_FINE_LOCATION")){
+            permisos.add("android.permission.ACCESS_FINE_LOCATION");
+        }
+        if(opCar.isChecked() && !checkedPermission("android.permission.CAMERA")){
+            permisos.add("android.permission.CAMERA");
+        }
+        if(opCon.isChecked() && !checkedPermission("android.permission.READ_CONTACTS")){
+            permisos.add("android.permission.READ_CONTACTS");
+        }
+        if(opPho.isChecked() && !checkedPermission("android.permission.CALL_PHONE")){
+            permisos.add("android.permission.CALL_PHONE");
+        }
+        if(opSto.isChecked() && !checkedPermission("android.permission.READ_EXTERNAL_STORAGE")){
+            permisos.add("android.permission.READ_EXTERNAL_STORAGE");
+        }
+
+        String arregloString[] = permisos.toArray(new String[permisos.size()]);
+
+        if(permisos.isEmpty()){
+            Intent intent = new Intent(this,permisos.class);
+            startActivity(intent);
+        }else{
+            requestPermission(arregloString);
+        }
+
+    }
+
+    public void CerrarApp(View view){
+        finish();
+    }
+
+    public void verificarPermisos(){
+        if(checkedPermission("android.permission.ACCESS_FINE_LOCATION")){
+            opLoc.setChecked(true);
+            opLoc.setClickable(false);
+        }
+        if(checkedPermission("android.permission.CAMERA")){
+            opCar.setChecked(true);
+            opCar.setClickable(false);
+        }
+        if(checkedPermission("android.permission.READ_CONTACTS")){
+            opCon.setChecked(true);
+            opCon.setClickable(false);
+
+        }
+        if(checkedPermission("android.permission.CALL_PHONE")){
+            opPho.setChecked(true);
+            opPho.setClickable(false);
+        }
+        if(checkedPermission("android.permission.READ_EXTERNAL_STORAGE")){
+            opSto.setChecked(true);
+            opSto.setClickable(false);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
         Intent intent = new Intent(this,permisos.class);
         startActivity(intent);
+
     }
+
+
 }
--- App2/app/src/main/java/com/example/apppermisos/permisos.java
+++ App2/app/src/main/java/com/example/apppermisos/permisos.java
@@ -1,14 +1,120 @@
 package com.example.apppermisos;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class permisos extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.util.Locale;
+
+public class permisos extends AppCompatActivity implements View.OnClickListener{
+
+    Button  blocation,bcamera,bstorage,bphone,bcontacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permisos);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        blocation = findViewById(R.id.blocation);
+        bcamera = findViewById(R.id.bcamara);
+        bstorage = findViewById(R.id.bstorage);
+        bphone = findViewById(R.id.bphone);
+        bcontacts = findViewById(R.id.bcontacts);
+
+        blocation.setOnClickListener(this);
+        bcamera.setOnClickListener(this);
+        bstorage.setOnClickListener(this);
+        bphone.setOnClickListener(this);
+        bcontacts.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view){
+        Intent intent;
+
+        switch (view.getId()){
+            case R.id.bcamara:
+                if(checkedPermission("android.permission.CAMERA")){
+                    intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(this,"Please request Permission", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.blocation:
+                if(checkedPermission("android.permission.ACCESS_FINE_LOCATION")){
+                    intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setPackage("com.google.android.apps.maps");
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(this,"Please request Permission", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.bstorage:
+                if(checkedPermission("android.permission.READ_EXTERNAL_STORAGE")){
+                    intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath() + "/myFolder/");
+                    intent.setDataAndType(uri, "*/*");
+                    startActivity(Intent.createChooser(intent, "Open folder"));
+
+                }else{
+                    Toast.makeText(this,"Please request Permission", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.bphone:
+                if(checkedPermission("android.permission.CALL_PHONE")){
+                    intent = new Intent(android.content.Intent.ACTION_CALL, Uri.parse("tel:8099040471"));
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(this,"Please request Permission", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.bcontacts:
+                if(checkedPermission("android.permission.READ_CONTACTS")){
+                    intent = new Intent(Intent.ACTION_PICK,  ContactsContract.Contacts.CONTENT_URI);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(this,"Please request Permission", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+
+    }
+
+    private boolean checkedPermission(String permiso){
+        int resultpermiss = ContextCompat.checkSelfPermission(getApplicationContext(),permiso);
+        return resultpermiss == PackageManager.PERMISSION_GRANTED ;
     }
 }
